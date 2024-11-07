a236389b421dda8e5ee91a5ad1a9b9a22d333046
harshapalnati <harshapalnati@gmail.com>
2024-11-07 15:54:32 -0500

    Set up spring project

---
+++ makrdown2pdf/src/main/java/com/makrdown2pdf/Makrdown2pdfApplication.java
@@ -0,0 +1,13 @@
+package com.makrdown2pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Makrdown2pdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Makrdown2pdfApplication.class, args);
+	}
+
+}
---
+++ makrdown2pdf/src/test/java/com/makrdown2pdf/Makrdown2pdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makrdown2pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Makrdown2pdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
